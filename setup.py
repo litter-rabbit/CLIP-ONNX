5328ab5ead232493064716af2cbf73d96ffd13fd
lrabbit <superaipro@ai.com>
2023-01-13 18:08:09 +0800

    remove requirement.txt

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 import pkg_resources
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as f:
-    install_requires = f.read().split("\n")
 
 setup(
     name="clip_onnx",
@@ -12,6 +10,5 @@ setup(
     description="",
     author="Maxim Gerasimov",
     packages=find_packages(),
-    install_requires=install_requires,
     include_package_data=True
 )
